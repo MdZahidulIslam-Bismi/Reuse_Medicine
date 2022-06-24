fe37b8ab369b28b5d4241a0e59eef1a86e357cac
Zahidul Islam <mdzahidulislam0184@gmail.com>
2022-06-24 20:06:47 +0600

    'some'

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -85,12 +85,16 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://codecasts.com.br">CODECASTS [pt-BR]</a>
+                    <a href="https://c   odecasts.com.br">CODECASTS [pt-BR]</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/codecasts/laravel">GitHub</a>
                 </div>
             </div>
+            <div class="container">
+                
+            </div>
         </div>
+
     </body>
 </html>
